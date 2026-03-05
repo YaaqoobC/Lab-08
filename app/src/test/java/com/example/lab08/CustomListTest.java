813fbde0f20281ec46034f98a4ad5e455a8a16d1
Yaaqoob Choulli <yaaqoobchoulli@Yaaqoobs-MacBook-Pro-4.local>
2026-03-04 17:45:23 -0700

    del, count ERROR

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,13 +1,33 @@
 package com.example.lab08;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
+
     @Test
     public void testHasCity() {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
-// This will fail initially because hasCity() doesn't  exist
         assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City city = new City("Edmonton", "AB");
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
+        list.deleteCity(city);
+        assertFalse(list.hasCity(city));
+    }
+
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        City city = new City("Edmonton", "AB");
+        list.addCity(city);
+        assertEquals(1, list.countCities());
     }
 }
